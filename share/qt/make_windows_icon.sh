0f92659703cdbc0ba6b785a02477577996c3acb6
Animal-Economy <info@aney.one>
2025-03-10 02:14:35 +0100

    v1.1

--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/tajcoin.ico
-
-convert ../../src/qt/res/icons/tajcoin-16.png ../../src/qt/res/icons/tajcoin-32.png ../../src/qt/res/icons/tajcoin-48.png ${ICON_DST}
